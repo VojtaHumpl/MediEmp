54dc09b50efe50b503b95a0ea286de35ce7f1bc4
VojtaHumpl <vojtech.humpl@tul.cz>
2021-11-28 19:41:40 +0100

    database driver class

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
-import java.sql.*;
 
 public class Main {
-	
-	private static Connection connect = null;
-	private static Statement statement = null;
-	private static ResultSet resultset = null;
+
 	
 	public static void main(String args[]) {
-		
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connect = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=admin&password=admin");
-			statement = connect.createStatement();
-			resultset = statement.executeQuery("select * from test.kurz");
-			while(resultset.next())
-				System.out.println(resultset.getString("nazev"));
+			var database = new DatabaseDriver();
+			database.Connect("localhost/test", "root", "admin");
+			var res = database.Query("select * from kurz");
+
+			while(res.next())
+				System.out.println(res.getString("nazev"));
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+
+
 		
 	}
 	
